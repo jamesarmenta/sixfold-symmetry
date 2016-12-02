7a39b3e8a556d0c316a885ef873a945533405648
James Armenta <armentajames@gmail.com>
2016-12-01 22:21:10 -0500

    Functioning Transitions ✨

--- templating/js/main.js
+++ templating/js/main.js
@@ -1,33 +1,52 @@
-$('.expanded-item-gallery').slick({
-  dots: true,
-  infinite: true,
-  speed: 250,
-  fade: true,
-  cssEase: 'linear',
-  centerMode: true,
-  arrows: false,
-});
+/*----------  ON DOCUMENT LOAD  ----------*/
 
-const items = Array.from(document.querySelectorAll('.item'));
+slickInit();
+updateItems();
 
-//for each item
-items.forEach(function(item) {
-  //click event handler 
-  item.addEventListener('click', function(event) {
-    event.preventDefault();
-    var item = event.target;
+/*----------  ON DOCUMENT UPDATE  ----------*/
 
-    //if it's not an item, then its a child
-    while (!item.classList.contains('item')) {
-      //find childs parent
-      item = item.parentNode;
-    } //'item' is now item div
+function documentUpdate(){
+  slickInit();
+  updateItems();
+}
 
-    expandSelectedItem(item);
-    return false;
+/*----------  GALLERY SLIDES  ----------*/
+
+function slickInit(){
+  $('.expanded-item-gallery').slick({
+    dots: true,
+    infinite: true,
+    speed: 250,
+    fade: true,
+    cssEase: 'linear',
+    centerMode: true,
+    arrows: false,
   });
-});
+}
 
+/*----------  HOME ITEMS  ----------*/
+
+function updateItems(){
+  const items = Array.from(document.querySelectorAll('.item'));
+
+  //for each item
+  items.forEach(function(item) {
+    //click event handler 
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      var item = event.target;
+
+      //if it's not an item, then its a child
+      while (!item.classList.contains('item')) {
+        //find childs parent
+        item = item.parentNode;
+      } //var item is now item div
+
+      disableScroll();
+      expandSelectedItem(item);
+    });
+  });
+}
 
 function expandSelectedItem(item) {
   //contains primary, secondary, tertiary divs
@@ -40,33 +59,99 @@ function expandSelectedItem(item) {
     var width = clone.style.width = existing.getPropertyValue('width');
     var height = clone.style.height = existing.getPropertyValue('height');
     clone.style.backgroundColor = existing.getPropertyValue('background-color');
-    clone.style.top = itemColors[i].getBoundingClientRect().top+window.scrollY+'px';
-    clone.style.left = itemColors[i].getBoundingClientRect().left+window.scrollX+'px';
+
+    var posTop = itemColors[i].getBoundingClientRect().top+window.scrollY;
+    var posLeft =itemColors[i].getBoundingClientRect().left+window.scrollX;
+
+    clone.style.top = posTop+'px';
+    clone.style.left = posLeft+'px';
     clone.style.position = 'absolute';
     clone.style.transform = 'scale(1)';
     //append clone to body
     document.body.appendChild(clone);
     //scale using window height or width?
-    var scaleWidth = window.innerWidth/width.replace('px','')*2;
-    var scaleHeight = window.innerHeight/height.replace('px','')*2;
+    var scaleWidth = (window.innerWidth+posLeft)/width.replace('px','')*2;
+    var scaleHeight = (window.innerHeight+posTop)/height.replace('px','')*2;
     var finalScale = (scaleHeight > scaleWidth) ? scaleHeight : scaleWidth;
     //how much should animation delay? 
     var delay = i/6;
     scaleItem(clone,finalScale,delay);
+
+    //on last one... do this
+    if(i==itemColors.length-1){
+      setTimeout(function() {
+        document.querySelector('body').style.backgroundColor = clone.style.backgroundColor;
+      }, delay+1000);
+      loadContentArea('/itempartial.html',delay+1000);
+    }
   }
-  
+
+}
+
+function loadContentArea (href,delay){
+  $('.content-area').addClass('fadeOut');
+  //TODO: Scroll to top
+  setTimeout(function() {
+    //TODO: fade out then in content area
+    $('.content-area').load(href, function(){
+      window.scrollTo(0,0);
+      enableScroll();
+      documentUpdate();
+      $('.content-area').removeClass('fadeOut');
+    });
+  }, delay);
 }
 
 // FIXME: Fix so that transform is cross-browser (moz, etc.)
-// FIXME: Update using CSS variables like --item-scale
-// FIXME: Animation jumps on iPhone
 function scaleItem(item,finalScale,delay){
   //OPENING ANIMATION
   window.setTimeout(function(){
-    document.body.style.position = 'fixed';
     item.style.transition = 'transform 1s ease-in-out '+delay+'s';
-    // document.documentElement.style.setProperty('--item-transform', 'scale('+finalScale+')');
     item.style.transform = 'scale('+finalScale+')';
   },0);
 
+  window.setTimeout(function(){
+    item.remove();
+  },delay+1000);
+
+}
+
+
+/*----------  HANDLERS  ----------*/
+
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+  // left: 37, up: 38, right: 39, down: 40,
+  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll(){
+  if (window.addEventListener) // older FF
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; // modern standard
+  window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+  window.ontouchmove  = preventDefault; // mobile
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll(){
+  if (window.removeEventListener){
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  }
+  window.onmousewheel = document.onmousewheel = null; 
+  window.onwheel = null; 
+  window.ontouchmove = null;  
+  document.onkeydown = null; 
 }
--- templating/public/js/main.js
+++ templating/public/js/main.js
@@ -1,29 +1,44 @@
-$('.expanded-item-gallery').slick({
-  dots: true,
-  infinite: true,
-  speed: 250,
-  fade: true,
-  cssEase: 'linear',
-  centerMode: true,
-  arrows: false,
-});
 
-const items = Array.from(document.querySelectorAll('.item'));
+slickInit();
+updateItems();
 
-items.forEach(function(item) {
-  item.addEventListener('click', function(event) {
-    event.preventDefault();
-    var item = event.target;
 
-    while (!item.classList.contains('item')) {
-      item = item.parentNode;
-    } 
+function documentUpdate(){
+  slickInit();
+  updateItems();
+}
 
-    expandSelectedItem(item);
-    return false;
+
+function slickInit(){
+  $('.expanded-item-gallery').slick({
+    dots: true,
+    infinite: true,
+    speed: 250,
+    fade: true,
+    cssEase: 'linear',
+    centerMode: true,
+    arrows: false,
   });
-});
+}
 
+
+function updateItems(){
+  const items = Array.from(document.querySelectorAll('.item'));
+
+  items.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      var item = event.target;
+
+      while (!item.classList.contains('item')) {
+        item = item.parentNode;
+      } 
+
+      disableScroll();
+      expandSelectedItem(item);
+    });
+  });
+}
 
 function expandSelectedItem(item) {
   var itemColors = item.querySelector('.item-colors').children;
@@ -34,25 +49,88 @@ function expandSelectedItem(item) {
     var width = clone.style.width = existing.getPropertyValue('width');
     var height = clone.style.height = existing.getPropertyValue('height');
     clone.style.backgroundColor = existing.getPropertyValue('background-color');
-    clone.style.top = itemColors[i].getBoundingClientRect().top+window.scrollY+'px';
-    clone.style.left = itemColors[i].getBoundingClientRect().left+window.scrollX+'px';
+
+    var posTop = itemColors[i].getBoundingClientRect().top+window.scrollY;
+    var posLeft =itemColors[i].getBoundingClientRect().left+window.scrollX;
+
+    clone.style.top = posTop+'px';
+    clone.style.left = posLeft+'px';
     clone.style.position = 'absolute';
     clone.style.transform = 'scale(1)';
     document.body.appendChild(clone);
-    var scaleWidth = window.innerWidth/width.replace('px','')*2;
-    var scaleHeight = window.innerHeight/height.replace('px','')*2;
+    var scaleWidth = (window.innerWidth+posLeft)/width.replace('px','')*2;
+    var scaleHeight = (window.innerHeight+posTop)/height.replace('px','')*2;
     var finalScale = (scaleHeight > scaleWidth) ? scaleHeight : scaleWidth;
     var delay = i/6;
     scaleItem(clone,finalScale,delay);
+
+    if(i==itemColors.length-1){
+      setTimeout(function() {
+        document.querySelector('body').style.backgroundColor = clone.style.backgroundColor;
+      }, delay+1000);
+      loadContentArea('/itempartial.html',delay+1000);
+    }
   }
 
-  }
+}
+
+function loadContentArea (href,delay){
+  $('.content-area').addClass('fadeOut');
+  setTimeout(function() {
+    $('.content-area').load(href, function(){
+      window.scrollTo(0,0);
+      enableScroll();
+      documentUpdate();
+      $('.content-area').removeClass('fadeOut');
+    });
+  }, delay);
+}
 
 function scaleItem(item,finalScale,delay){
   window.setTimeout(function(){
-    document.body.style.position = 'fixed';
     item.style.transition = 'transform 1s ease-in-out '+delay+'s';
     item.style.transform = 'scale('+finalScale+')';
   },0);
 
+  window.setTimeout(function(){
+    item.remove();
+  },delay+1000);
+
+}
+
+
+
+
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+    e.preventDefault();
+  e.returnValue = false;  
+}
+
+function preventDefaultForScrollKeys(e) {
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+function disableScroll(){
+  if (window.addEventListener) 
+    window.addEventListener('DOMMouseScroll', preventDefault, false);
+  window.onwheel = preventDefault; 
+  window.onmousewheel = document.onmousewheel = preventDefault; 
+  window.ontouchmove  = preventDefault; 
+  document.onkeydown  = preventDefaultForScrollKeys;
+}
+
+function enableScroll(){
+  if (window.removeEventListener){
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  }
+  window.onmousewheel = document.onmousewheel = null; 
+  window.onwheel = null; 
+  window.ontouchmove = null;  
+  document.onkeydown = null; 
 }
